3ed10cbcc9f030acf884c0292bf563e26f943e83
MohanadOmarrr <mohanadfreelancing@gmail.com>
2022-09-14 18:45:01 +0200

    all commits

--- manage.py
+++ manage.py
@@ -1,3 +1,22 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:7328c00690004a3a13c974ea6d8e83a7f5d86dc194acf4221ea75507edc8bce1
-size 692
+#!/usr/bin/env python
+"""Django's command-line utility for administrative tasks."""
+import os
+import sys
+
+
+def main():
+    """Run administrative tasks."""
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'kadell_website.settings')
+    try:
+        from django.core.management import execute_from_command_line
+    except ImportError as exc:
+        raise ImportError(
+            "Couldn't import Django. Are you sure it's installed and "
+            "available on your PYTHONPATH environment variable? Did you "
+            "forget to activate a virtual environment?"
+        ) from exc
+    execute_from_command_line(sys.argv)
+
+
+if __name__ == '__main__':
+    main()
