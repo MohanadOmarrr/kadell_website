4e1f32b4640b429d9dc8adf0594c8208b8bfdb69
MohanadOmarrr <mohanadfreelancing@gmail.com>
2022-09-14 23:28:48 +0200

    settings.py

--- kadell_website/settings.py
+++ kadell_website/settings.py
@@ -142,13 +142,13 @@ USE_TZ = True
 
 BASE_DIR = "/home/project-root/"
 
-STATIC_URL = "/static/"
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATIC_URL = "/kadell-sa.com/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
-STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
+STATICFILES_DIRS = (os.path.join(BASE_DIR, "staticfiles"),)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
