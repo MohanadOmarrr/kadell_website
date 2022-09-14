fef36ab8f0c4f30e28d1d51fe54da4979512b47f
MohanadOmarrr <mohanadfreelancing@gmail.com>
2022-09-14 23:38:03 +0200

    settings.py

--- kadell_website/settings.py
+++ kadell_website/settings.py
@@ -139,10 +139,7 @@ USE_TZ = True
 #
 # MEDIA_URL = '/media/'
 # MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
-
-BASE_DIR = "/home/project-root/"
-
-STATIC_URL = "/kadell-sa.com/static/"
+STATIC_URL = "/static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 MEDIA_URL = "/media/"
