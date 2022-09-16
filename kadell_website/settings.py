917d917c27e2fe564823c5433c3f7a20ae4c7d3e
MohanadOmarrr <mohanadfreelancing@gmail.com>
2022-09-16 17:23:30 +0200

    static files

--- kadell_website/settings.py
+++ kadell_website/settings.py
@@ -126,16 +126,15 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
-# STATICFILES_DIRS = [
-#     os.path.join(BASE_DIR, "kadell_website/static")
-# ]
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "kadell_website/static")
+]
 #
 # STATIC_URL = '/static/'
 # STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 #
 # MEDIA_URL = '/media/'
 # MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
-BASE_DIR = "/home/project-root"
 
 STATIC_URL = "http://kadell-sa.com/static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
@@ -143,7 +142,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, "static")
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
-STATICFILES_DIRS = (os.path.join(BASE_DIR, "staticfiles"),)
+# STATICFILES_DIRS = (os.path.join(BASE_DIR, "staticfiles"),)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
