ae630a90270c359f657b1940b24193952b7c1b17
MohanadOmarrr <mohanadfreelancing@gmail.com>
2022-09-22 21:07:52 +0200

    services functionality

--- services/views.py
+++ services/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Service, ServicesProducts
+from .models import Service, Products
 
 
 # Create your views here.
@@ -11,5 +11,5 @@ def services(request):
 def service_details(request, service_id):
     all_services = Service.objects.all
     service = get_object_or_404(Service, pk=service_id)
-    products = get_object_or_404(ServicesProducts, pk=service_id)
+    products = get_object_or_404(Products, pk=service_id)
     return render(request, 'services.html', {'all_services': all_services, 'service': service, 'all_products': [products]})
