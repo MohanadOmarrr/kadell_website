284a93893500c7b4d94964ea543f5633842c0f7b
MohanadOmarrr <mohanadfreelancing@gmail.com>
2022-09-23 03:38:45 +0200

    service functionality

--- services/models.py
+++ services/models.py
@@ -6,6 +6,7 @@ class Service(models.Model):
     title = models.CharField(max_length=100, default=0)
     body = models.TextField(max_length=1000, null=True, blank=True, default=0)
     image = models.ImageField(upload_to='images/', default=0)
+
     def products_list(self):
         return self.products.split(',')
 
