601356f52165fce3088c87fb56e206c1234a3563
root <root@localhost>
2022-09-23 01:16:37 +0000

    all

--- services/models.py
+++ services/models.py
@@ -6,9 +6,6 @@ class Service(models.Model):
     title = models.CharField(max_length=100, default=0)
     body = models.TextField(max_length=1000, null=True, blank=True, default=0)
     image = models.ImageField(upload_to='images/', default=0)
-    icon = models.ImageField(upload_to='images/', default=0)
-    products = models.TextField(max_length=1000, default=0)
-    prices = models.TextField(max_length=1000, default=0)
 
     def products_list(self):
         return self.products.split(',')
