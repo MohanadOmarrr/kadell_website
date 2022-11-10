9fc563cb623fd4dd4d2c043b7ab1eb415d3efc7e
MohanadOmarrr <mohanadfreelancing.com>
2022-11-10 03:02:22 +0200

    all

--- services/models.py
+++ services/models.py
@@ -21,7 +21,6 @@ class Service(models.Model):
 class ServicesProduct(models.Model):
     service = models.ForeignKey(Service, on_delete=models.CASCADE)
     title = models.CharField(max_length=100, default=0)
-    image = models.ImageField(upload_to='images/', default=0)
     prices = models.TextField(max_length=1000, default=0)
 
     def __str__(self):
